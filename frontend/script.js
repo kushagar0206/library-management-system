17cb2d1837f78b4f4c8fa97d450bcca951004de8
Kushagar <kushagar2003@gmail.com>
2026-04-20 07:39:08 +0530

    Implement login validation on button click
    Add event listener for button click to validate login.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,18 @@
-function a() {
-  let uname = document.querySelector("#username");
-  alert(uname);
-  let sum = 5 + 6;
-  console.log("Kushagar");
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  checker();
+});
+
+function checker() {
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+
+  if (email === "admin@2026" && password === "admin4124") {
+    window.location.href = "main.html";
+  } else {
+    let add = document.querySelector(".form-div");
+    let error = document.createElement("p");
+    error.innerText = "Invalid email or password";
+    add.insertAdjacentElement("beforeend", error);
+  }
 }
